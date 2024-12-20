4fd1dd38ac219087d68f58b95fdb9a9edbe7b5b6
Erick Kamanda <info.proresearchers@gmail.com>
2024-12-20 13:33:57 +0300

    Design and Style the Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
 import { Outlet} from 'react-router-dom';
+import Header from "./components/Header";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
       <main>
+        <Header />
+        <div className='pt-16'>
         <Outlet/>
+        </div>
+        <Footer />
       </main>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import logo from "../assets/logo.png"
+import userIcon from "../assets/user.svg"
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  const navigation = [
+    {
+      label: "Movies",
+      href: "movies"
+    },
+    {
+      label: "TV Shows",
+      href: "tv"
+    }
+  ];
+  return (
+   <header className='fixed top-0 w-full h-16 bg-black bg-opacity-70'>
+      <div className='container mx-auto px-3 flex items-center h-full'>
+        <div>
+          <img src={logo} alt="Cinemate Logo" width={120}/>
+        </div>
+      
+        <nav className='hidden lg:flex items-center gap-3 ml-5'>
+          {
+            navigation.map((nav, index) => {
+              return (
+                <div>
+                  <NavLink 
+                  key={nav.label} 
+                  to={nav.href} 
+                  className={({isActive}) => `px-2 hover:text-red-500 {isActive && text-red-500}`}
+                  >
+                    {nav.label}
+                  </NavLink>
+                </div>
+              )
+            })
+          }
+        </nav>
+        <div className='ml-auto'>
+          <div>
+            <img src={userIcon} alt="" className='w-10 h-10'/>
+          </div>
+        </div>
+      </div>
+   </header>
+  )
+}
+
+export default Header;
