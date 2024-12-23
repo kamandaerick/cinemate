7c951f19f776e44bcf728cfd6d521ef91ec8f514
Erick Kamanda <info.proresearchers@gmail.com>
2024-12-23 21:22:39 +0300

    complete navigation

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DetailsPage = () => {
   return (
     <div>
-      Movies and Series Details Page
+      
     </div>
   )
 }
--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ExplorePage = () => {
   return (
     <div>
-      Explore Movies and Series
+     
     </div>
   )
 }
