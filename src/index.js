6944c50272f5a34f54e1bdabe7e1c94f2f31632f
Erick Kamanda <info.proresearchers@gmail.com>
2024-12-27 19:30:13 +0300

    Create store and import it in the index.js file

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
 import router from "./routes";
 import axios from 'axios';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 // Set Up Axios
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
@@ -13,9 +15,11 @@ axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <RouterProvider router={router}/>
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
