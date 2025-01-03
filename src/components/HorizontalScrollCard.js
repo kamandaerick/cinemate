b64307de11ed325c724cb6bb3a12829d0240c7a1
Erick Kamanda <info.proresearchers@gmail.com>
2025-01-03 15:42:42 +0300

    Implement smooth scrolling

--- src/components/HorizontalScrollCard.js
+++ src/components/HorizontalScrollCard.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 import { GrNext } from "react-icons/gr";
 import { GrPrevious } from "react-icons/gr";
 
 const HorizontalScrollCard = ({data=[], heading}) => {
+  const containerRef = useRef()
+  // Handle next button
+  const handleNext = () => {
+    containerRef.current.scrollLeft += 300;
+  }
+  // Handle previous button
+  const handlePrevious = () => {
+    containerRef.current.scrollLeft -= 300;
+  }
   return (
     <div>
         <div className=' container mx-auto my-10 px-3 '>
           <h2 className='text-2xl text-white font-bold mb-2'>{heading}</h2>
-          <div className='overflow-hidden relative'>
-            <div className= "grid gap-3 grid-flow-col overflow-x-scroll z-10 relative">
+          <div  className='overflow-hidden relative'>
+            <div ref={containerRef} className= "grid gap-3 grid-flow-col overflow-x-scroll z-10  scroll-smooth transition-all">
               {
               data.map((data, index) => {
                 return (
@@ -25,10 +34,10 @@ const HorizontalScrollCard = ({data=[], heading}) => {
             }
             </div>
               <div className='absolute flex top-0 justify-between w-full h-full items-center z-100'>
-              <button className='bg-white p-1 rounded-full z-10 text-black'>
+              <button onClick={handlePrevious} className='bg-white p-1 rounded-full z-10 text-black'>
                 <GrPrevious size={24}/>
               </button>
-              <button className='bg-white p-1 rounded-full z-10 text-black'>
+              <button onClick={handleNext} className='bg-white p-1 rounded-full z-10 text-black'>
                 <GrNext size={24}/>
               </button>
             </div>
