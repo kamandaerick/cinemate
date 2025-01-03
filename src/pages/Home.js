15a0c31fb089a740100dc9889ad3bb9a2148b6e6
Erick Kamanda <info.proresearchers@gmail.com>
2025-01-03 19:59:54 +0300

    Add two horizontal sections for Populat tv series and trending movies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,28 +8,55 @@ import axios from 'axios';
 const Home = () => {
   const allTrending = useSelector((state) => state.cinemateData.bannerData.results);
   const [topRatedSeries, setTopRatedSeries] = useState([]);
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+  const [popularSeries, setPopularSeries] = useState([]);
 
+  // Fetch Top Rated Tv Series
   const fetchTopRatedSeries = async () => {
     try {
       const res = await axios.get('/tv/top_rated');
       setTopRatedSeries(res.data.results)
-      console.log("Top Rated Series: ", res)
     } catch (error) {
       console.log("Error: ", error)
     }
   }
 
+  // Fetch Top Rated Movies
+  const fetchTopRatedMovies = async () => {
+    try {
+      const res = await axios.get('/trending/movie/week');
+      setTopRatedMovies(res.data.results)
+      // console.log("Top Movies: ", res)
+    } catch (error) {
+      console.log("Error: ", error)
+    }
+  }
+
+  // fetch Most popular Tv Series 
+  const fetchPopularSeries = async () => {
+    try {
+      const res = await axios.get('/tv/popular');
+      setPopularSeries(res.data.results)
+      // console.log("Popular Series: ", res)
+    } catch (error) {
+      console.log("Error: ", error)
+    }
+  }
   useEffect(() => {
     fetchTopRatedSeries()
+    fetchTopRatedMovies()
+    fetchPopularSeries()
   }, [])
   
-  // console.log("All Trending", allTrending)
+  
   if (!allTrending) return <div>Loading...</div>;
   return (
     <div>
       <HomeBanner />
       <HorizontalScrollCard data={allTrending} heading={'Trending Now'} trending={true} />
       <HorizontalScrollCard data={topRatedSeries} heading={'Top Rated Tv Series'} trending={false}/>
+      <HorizontalScrollCard data={topRatedMovies} heading={'Trending Movies'} trending={true}/>
+      <HorizontalScrollCard data={popularSeries} heading={'Popular Tv Series'} trending={false}/>
     </div>
   )
 };
