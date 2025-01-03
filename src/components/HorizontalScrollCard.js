d69b0460d0b784f668b5fe302e8141b2a55aa003
Erick Kamanda <info.proresearchers@gmail.com>
2025-01-03 14:47:29 +0300

    Add previous and nex buttons to the horizontal scroll

--- src/components/HorizontalScrollCard.js
+++ src/components/HorizontalScrollCard.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Card from './Card'
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
 
 const HorizontalScrollCard = ({data=[], heading}) => {
   return (
     <div>
-        <div className='container mx-auto my-10 px-3'>
+        <div className=' container mx-auto my-10 px-3 '>
           <h2 className='text-2xl text-white font-bold mb-2'>{heading}</h2>
-          <div className='overflow-hidden'>
-            <div className= "grid gap-3 grid-flow-col overflow-x-scroll">
+          <div className='overflow-hidden relative'>
+            <div className= "grid gap-3 grid-flow-col overflow-x-scroll z-10 relative">
               {
               data.map((data, index) => {
                 return (
@@ -22,7 +24,16 @@ const HorizontalScrollCard = ({data=[], heading}) => {
               })
             }
             </div>
+              <div className='absolute flex top-0 justify-between w-full h-full items-center z-100'>
+              <button className='bg-white p-1 rounded-full z-10 text-black'>
+                <GrPrevious size={24}/>
+              </button>
+              <button className='bg-white p-1 rounded-full z-10 text-black'>
+                <GrNext size={24}/>
+              </button>
+            </div>
           </div>
+
       </div>
     </div>
   )
