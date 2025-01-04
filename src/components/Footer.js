e18556708430415ccb19a8825c323f98dadfba2e
Erick Kamanda <info.proresearchers@gmail.com>
2025-01-04 11:08:06 +0300

    Add my social links on the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { GrLinkedin } from "react-icons/gr";
 import { FaGithubSquare } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+
+const LINKEDIN_PROFILE = 'https://www.linkedin.com/in/erick-kamanda/'
+const GITHUB_PROFILE = 'https://github.com/kamandaerick'
 
 const Footer = () => {
   return (
@@ -8,14 +13,18 @@ const Footer = () => {
     <div className=''>
       <p className='text-white text-lg pb-3'>Designed By Erick Kamanda</p>
       <div className='flex items-center justify-center gap-10'>
-        <div className='flex items-center gap-3'>
-          <p>Linkedin:</p>
-         <GrLinkedin />
-        </div>
-        <div className='flex items-center gap-3'>
-        <p>GitHub</p>
-           <FaGithubSquare />
-        </div>
+        <Link to={LINKEDIN_PROFILE}>
+          <div className='flex items-center gap-3' target="_blank" rel="noopener noreferrer">
+            <p>Linkedin:</p>
+          <GrLinkedin />
+          </div>
+        </Link>
+        <Link to={GITHUB_PROFILE}>
+          <div className='flex items-center gap-3' target="_blank" rel="noopener noreferrer">
+          <p>GitHub</p>
+            <FaGithubSquare />
+          </div>
+        </Link>
       </div>
     </div>
    </footer>
