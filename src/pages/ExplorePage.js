29e7075568a6cb3202218652d42407a076738e4b
Erick Kamanda <info.proresearchers@gmail.com>
2025-01-06 11:45:50 +0300

    Display movies and tv shows on their respective pages

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,48 +1,62 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Card from '../components/Card';
 
 const ExplorePage = () => {
-  const params = useParams()
-  const [pageNo, setPageNo] = useState(1)
-  const [data, setData] = useState([])
-  const [totalPageNos, setTotalPageNos] = useState(1)
-  
+  const params = useParams();
+  const [pageNo, setPageNo] = useState(1);
+  const [data, setData] = useState([]);
+  const [totalPageNos, setTotalPageNos] = useState(1);
+
+  // Reset data when params.explore changes
+  useEffect(() => {
+    setData([]);
+  }, [params.explore]);
+
+  // Fetch data
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`/discover/${params.explore}`, {params: {page: pageNo}});
+        const response = await axios.get(`/discover/${params.explore}`, { params: { page: pageNo } });
         console.log('Data', response);
-        
-        setData(prev => {
-          return [...prev, ...response.data.results]
-        })
 
-        setTotalPageNos(response.data.total_pages)
+        setData(prev => [...prev, ...response.data.results]); // Append new data
+        setTotalPageNos(response.data.total_pages);
       } catch (error) {
         console.log('Error', error);
       }
     };
 
-
     fetchData();
   }, [params.explore, pageNo]);
 
-      // Handle scroll (infinite scroll)
-  const handleInfinitescroll = () => {
-    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      setPageNo(prev => prev += 1)
-    }
-  }
-
+  // Handle scroll (infinite scroll)
   useEffect(() => {
-    window.addEventListener('scroll', handleInfinitescroll)
-  }, [])
+    const handleInfinitescroll = () => {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+        setPageNo(prev => prev + 1);
+      }
+    };
+
+    window.addEventListener('scroll', handleInfinitescroll);
+    return () => {
+      window.removeEventListener('scroll', handleInfinitescroll);
+    };
+  }, []);
+
   return (
     <div>
-     <h2 className='text-2xl font-semibold capitalize'>Explore {params.explore}</h2>
+      <div className="container mx-auto">
+        <h2 className="text-2xl font-semibold capitalize my-3">Explore {params.explore}</h2>
+        <div>
+          {data.map((exploreData, index) => (
+            <Card data={exploreData} key={`${exploreData.id}-${params.explore}-${index}`} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ExplorePage;
