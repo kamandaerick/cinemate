bbca842a2b752ae2997f40b162e82e43bd789b8f
Erick Kamanda <info.proresearchers@gmail.com>
2025-01-06 11:12:24 +0300

    Create an infinite scroll

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -1,27 +1,46 @@
 import axios from 'axios';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 const ExplorePage = () => {
   const params = useParams()
+  const [pageNo, setPageNo] = useState(1)
+  const [data, setData] = useState([])
+  const [totalPageNos, setTotalPageNos] = useState(1)
   
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`/discover/${params.explore}`)
-      console.log('Data', response)
-     
-    } catch (error) {
-      console.log('Error', error)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`/discover/${params.explore}`, {params: {page: pageNo}});
+        console.log('Data', response);
+        
+        setData(prev => {
+          return [...prev, ...response.data.results]
+        })
+
+        setTotalPageNos(response.data.total_pages)
+      } catch (error) {
+        console.log('Error', error);
+      }
+    };
+
+
+    fetchData();
+  }, [params.explore, pageNo]);
+
+      // Handle scroll (infinite scroll)
+  const handleInfinitescroll = () => {
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      setPageNo(prev => prev += 1)
     }
   }
 
-
   useEffect(() => {
-    fetchData()
-  },)
+    window.addEventListener('scroll', handleInfinitescroll)
+  }, [])
   return (
     <div>
-     
+     <h2 className='text-2xl font-semibold capitalize'>Explore {params.explore}</h2>
     </div>
   )
 }
