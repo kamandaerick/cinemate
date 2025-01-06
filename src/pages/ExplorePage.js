fb458117c8d122ca7f7fd315399a479bda795972
Erick Kamanda <info.proresearchers@gmail.com>
2025-01-06 11:53:37 +0300

    Set the display to flex and wrap

--- src/pages/ExplorePage.js
+++ src/pages/ExplorePage.js
@@ -49,7 +49,7 @@ const ExplorePage = () => {
     <div>
       <div className="container mx-auto">
         <h2 className="text-2xl font-semibold capitalize my-3">Explore {params.explore}</h2>
-        <div>
+        <div className='flex flex-wrap '>
           {data.map((exploreData, index) => (
             <Card data={exploreData} key={`${exploreData.id}-${params.explore}-${index}`} />
           ))}
